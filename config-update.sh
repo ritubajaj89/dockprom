8c0156d8f7120d17b275f70c5dd7c85f12215d81
Bajaj, Ritu1 <Ritu.Bajaj2@adidas.com>
2022-06-17 11:17:59 +0530

    fixing mv

--- config-update.sh
+++ config-update.sh
@@ -37,37 +37,37 @@ for a in $prom_list; do
    continue      
    fi	
 done
-# Checking configuration for alertmanager
-cd $tmp_file_location/$date/alertmanager
-diff alertmanager.yml $prom_config_location/alertmanager.yml > /dev/null
-   if [[ "$?" == "1" ]]; then
-        # File exists but is different so copy changed file
-        mv $prom_config_location/alertmanager.yml $prom_config_location/alertmanager.yml_bkp_$date
-        cp alertmanager.yml $prom_config_location
-		  amtool check-config /etc/config/prometheus/alertmanager.yml | grep -i 'SUCCESS'
-        if [[ "$?" == "1" ]]; then
-        	echo "Configuration file is invalid"
-        fi	
-        systemctl restart alertmanager 
-        echo "Alertmanager Service has been restarted"
-    continue  
-   fi		
-# Checking configuration for yace
-cd $tmp_file_location/$date/yace
-yace_list=`find . -type f -printf "%f\n"`
-for i in $yace_list; do
-   if [ ! -f "$yace_config_location/$i" ]; then
-        cp $i $yace_config_location
-      continue
-   fi
-   if [ -f "$yace_config_location/$i" ]; then
-	   diff $i $yace_config_location/$i > /dev/null
-  	   if [[ "$?" == "1" ]]; then
-    	    # File exists but is different so copy changed file
-        	mv $yace_config_location/$i $yace_config_location/$i_bkp_$date
-        	cp $i $yace_config_location       	
-        	echo "Config will be reloaded on next 10 min"
-   	   fi
-    continue   
-   fi	
-done
+# # Checking configuration for alertmanager
+# cd $tmp_file_location/$date/alertmanager
+# diff alertmanager.yml $prom_config_location/alertmanager.yml > /dev/null
+#    if [[ "$?" == "1" ]]; then
+#         # File exists but is different so copy changed file
+#         mv $prom_config_location/alertmanager.yml $prom_config_location/alertmanager.yml_bkp_$date
+#         cp alertmanager.yml $prom_config_location
+# 		  amtool check-config /etc/config/prometheus/alertmanager.yml | grep -i 'SUCCESS'
+#         if [[ "$?" == "1" ]]; then
+#         	echo "Configuration file is invalid"
+#         fi	
+#         systemctl restart alertmanager 
+#         echo "Alertmanager Service has been restarted"
+#     continue  
+#    fi		
+# # Checking configuration for yace
+# cd $tmp_file_location/$date/yace
+# yace_list=`find . -type f -printf "%f\n"`
+# for i in $yace_list; do
+#    if [ ! -f "$yace_config_location/$i" ]; then
+#         cp $i $yace_config_location
+#       continue
+#    fi
+#    if [ -f "$yace_config_location/$i" ]; then
+# 	   diff $i $yace_config_location/$i > /dev/null
+#   	   if [[ "$?" == "1" ]]; then
+#     	    # File exists but is different so copy changed file
+#         	mv $yace_config_location/$i $yace_config_location/$i_bkp_$date
+#         	cp $i $yace_config_location       	
+#         	echo "Config will be reloaded on next 10 min"
+#    	   fi
+#     continue   
+#    fi	
+# done
